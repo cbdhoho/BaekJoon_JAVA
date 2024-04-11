33f464f3f4e30fb4439b0d856435e8e5e1edf83a
Min Seon <81568368+cbdhoho@users.noreply.github.com>
2024-04-12 00:07:30 +0900

    Update A＋B － 4.java
    EOF 추가설명

--- 백준/Bronze/10951. A＋B － 4/A＋B － 4.java
+++ 백준/Bronze/10951. A＋B － 4/A＋B － 4.java
@@ -13,3 +13,6 @@ public class Main {
 
     }
 }
+
+//EOF (End Of File)을 알려주기 위해 만든 문제. hasNext()를 써서 다음에 입력되는 데이터가 있는지 확인하고 없으면 종료한다. 
+//이때, 난 scanner를 사용했는데, scanner에서는 입력되는 데이터가 없으면 NoSuchElementException이 출력된다.
