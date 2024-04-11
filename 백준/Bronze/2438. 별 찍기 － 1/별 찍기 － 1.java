f2710c6228a17f877e28609c328c73f65b8d3f1f
Min Seon <81568368+cbdhoho@users.noreply.github.com>
2024-04-11 23:18:09 +0900

    [Bronze V] Title: 별 찍기 - 1, Time: 312 ms, Memory: 17940 KB -BaekjoonHub

---
+++ 백준/Bronze/2438. 별 찍기 － 1/별 찍기 － 1.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        String s = "*";
+
+        for(int i=1;i<=n;i++){
+            for(int j=0;j<i;j++){
+                System.out.print(s);
+            }
+            if(i==n){
+                break;
+            }else {
+                System.out.println();
+            }
+        }
+
+    }
+}
